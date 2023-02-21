f5ff795990b2552aaee5148a3d8920d14b61be48
Guilhem Barboux <guilhembarboux@gmail.com>
2023-02-21 18:05:34 +0100

    Chore(typescript): add path to component folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      '@components' : path.resolve(__dirname, './src/components'),
+      '@elements' : path.resolve(__dirname, './src/elements')
+    },
+  },
   plugins: [react()],
 })
