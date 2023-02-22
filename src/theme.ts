20d6ddb10e0d7c149e95792fd45fb2285501d3f1
Guilhem Barboux <guilhembarboux@gmail.com>
2023-02-22 15:10:26 +0100

    Styles(Table): define table default style

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,6 @@ import type { Theme } from 'theme-ui'
 import preset from '@theme-ui/preset-system'
 
 export const theme: Theme = {
-  ...preset
   ...preset,
   config: {
     useColorSchemeMediaQuery: false,
@@ -10,10 +9,50 @@ export const theme: Theme = {
     initialColorModeName: 'deep',
     printColorModeName: 'default',
   },
+  buttons: {
+    secondary: {
+      border: '1px solid',
+      color: 'primary',
+      bg: 'background',
+      cursor: "pointer"
+    },
+  },
   layout: {
     container: {
       maxWidth: 1024,
       py: 2
     }
   },
+  styles: {
+    ...preset.styles,
+    table: {
+      width: '100%',
+      my: 4,
+      borderCollapse: 'separate',
+      borderSpacing: 0,
+      tr: {
+        '&.is-clickable': {
+          cursor: 'pointer',
+          '&:hover': {
+            bg: 'highlight'
+          }
+        }
+      },
+      'th,td': {
+        textAlign: 'left',
+        p: 2,
+        borderColor: 'primary',
+        borderBottomStyle: 'solid',
+      },
+      th: {
+        verticalAlign: 'bottom',
+        borderBottomWidth: '2px',
+        backgroundColor: 'primary',
+      },
+      td: {
+        verticalAlign: 'top',
+        borderBottomWidth: '1px',
+      }
+    },
+  }
 }
